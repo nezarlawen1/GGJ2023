e76ae14dadcd37c4f77ca1b7af49e9ebec985272
nezarlawen1 <43337135+nezarlawen1@users.noreply.github.com>
2023-02-02 20:13:46 +0200

    Created player controller and scout

---
+++ GGJ2023/Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.PlayerActions player;
+
+    private PlayerMotor motor;
+    private PlayerLook look;
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        player = playerInput.Player;
+
+        motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+
+    }
+    private void FixedUpdate()
+    {
+        motor.ProcessMove(player.Movement.ReadValue<Vector2>());
+    }
+    private void LateUpdate()
+    {
+        look.ProcessLook(player.Look.ReadValue<Vector2>());
+    }
+    public void OnEnable()
+    {
+        player.Enable();
+    }
+    public void OnDisable()
+    {
+        player.Disable();
+    }
+}
