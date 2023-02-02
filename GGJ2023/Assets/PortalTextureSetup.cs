d43019ac19f88592f1e910fc77123723a9846432
RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
2023-02-02 23:10:48 +0200

    Squashed commit of the following:
    commit ee0bfbe4e50388b7517ffa44e88eb0d85507233e
    Author: RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
    Date:   Thu Feb 2 23:10:32 2023 +0200
    
        Portals (Done) (Prefabbed)
    
    commit 8bb1d981de0c7a5a451747ad08a4562df8fd1202
    Merge: b84084d 84ef027
    Author: RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
    Date:   Thu Feb 2 20:32:02 2023 +0200
    
        Merge branch 'main' into Robby
    
    commit b84084d30ce92fe68e70569e8ffb7bf06bfc1d2d
    Author: RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
    Date:   Thu Feb 2 20:31:39 2023 +0200
    
        Returning Scene
    
    commit 7a0a4411cc851a3802166be4ae798ecdcd11a0ce
    Author: RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
    Date:   Thu Feb 2 20:19:01 2023 +0200
    
        Revert "Squashed commit of the following:"
    
        This reverts commit 500806148541eb6b291ced77088bc413aa3bba00.
    
    commit c9bcc7ccd7e1827166607bd554ca3d466ae9db02
    Merge: 66b8a2a 5008061
    Author: RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
    Date:   Thu Feb 2 20:16:09 2023 +0200
    
        Merge branch 'main' into Robby
    
    commit 66b8a2ab0053af5c2e1594b54584ed39e8a66b84
    Author: RobbyRicch <87952183+RobbyRicch@users.noreply.github.com>
    Date:   Thu Feb 2 20:13:14 2023 +0200
    
        Started Work On Portal Scene.

---
+++ GGJ2023/Assets/PortalTextureSetup.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera cameraA;
+    public Material cameraMatA;
+
+    public Camera cameraB;
+    public Material cameraMatB;
+
+    void Start()
+    {
+        if (cameraA.targetTexture != null)
+        {
+            cameraA.targetTexture.Release();
+
+        }
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
+
+        if (cameraB.targetTexture != null) 
+        {
+            cameraB.targetTexture.Release();
+
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width , Screen.height , 24);
+        cameraMatB.mainTexture = cameraB.targetTexture;
+    }
+
+  
+}
