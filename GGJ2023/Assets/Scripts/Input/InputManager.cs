859f1fafae24bb9c4ee7b312ca1dc4c5b62b85f1
George Rashed <goerge1110@gmail.com>
2023-02-04 00:18:27 +0200

    Squashed commit of the following:
    commit 751783842b6398aae8819bddecf55305d8d90a07
    Author: George Rashed <goerge1110@gmail.com>
    Date:   Sat Feb 4 00:18:06 2023 +0200
    
        Update InputManager.cs

--- GGJ2023/Assets/Scripts/Input/InputManager.cs
+++ GGJ2023/Assets/Scripts/Input/InputManager.cs
@@ -78,6 +78,7 @@ public class InputManager : MonoBehaviour
                         if (!GameManager.Instance.CurrentCore.Purified)
                         {
                             GameManager.Instance.CurrentCore.PurifyCore();
+                            GameManager.Instance.CurrentMaze.SwitchVision(false);
                         }
                     }
                 }
