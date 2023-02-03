dd6c2f1fdf4ea913ad61ea2b9a52826314e1876c
George Rashed <goerge1110@gmail.com>
2023-02-04 00:59:25 +0200

    Update PortalPointer.cs

--- GGJ2023/Assets/Scripts/Utilities/PortalPointer.cs
+++ GGJ2023/Assets/Scripts/Utilities/PortalPointer.cs
@@ -6,12 +6,6 @@ public class PortalPointer : MonoBehaviour
 {
     [SerializeField] private Transform _portal;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
